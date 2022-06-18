b3bf309d4b793f9a1511c97deb326c3a9bc6f07d
Elías <eliasmora94@gmail.com>
2022-06-18 13:17:04 -0400

    expand and remove buttons functionalities

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+
+export interface DashboardCard {
+  id: number;
+  title: string;
+  cols: number;
+  rows: number;
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -8,26 +16,26 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+  cards: DashboardCard[] = [
+    { id: 1, title: 'Card 1', cols: 2, rows: 1 },
+    { id: 2, title: 'Card 2', cols: 1, rows: 1 },
+    { id: 3, title: 'Card 3', cols: 1, rows: 2 },
+    { id: 4, title: 'Card 4', cols: 1, rows: 1 }
+  ];
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
+  /** Based on the screen size, switch from standard to one column per row */
+  isHandheld$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => matches)
   );
 
   constructor(private breakpointObserver: BreakpointObserver) { }
+
+  onRemove(card: DashboardCard): void {
+    this.cards = this.cards.filter(c => c.id !== card.id);
+  }
+
+  onExpand(card: DashboardCard): void {
+    card.cols = 2;
+    card.rows = 2;
+  }
 }
